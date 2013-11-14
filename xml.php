8bb98fb956590af8451cf0e9a1ea8451a7476843
Bernadette Vina <vivisutoyo@hotmail.com>
2013-11-14 11:29:19 +0700

    all files has been added

---
+++ xml.php
@@ -0,0 +1,6 @@
+<?php
+header("Content-Type: text/xml");
+echo file_get_contents("http://localhost/webservice/DataXML.xml");
+//sumber http://www.w3schools.com/php/func_filesystem_file_get_contents.asp
+?>
+
